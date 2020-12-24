462d21fde394bd9659dbfc66b3e4f614414e5b30
Rahul Ajmani <rahulajmani910@gmail.com>
2020-12-24 14:21:38 +0530

    react hooks and todos

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
 import './App.css';
-import {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
+import TodoList from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todo';
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([])
+  const inputNameVal = useRef()
 
-  let updateCount = (value) => setCount(value);
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(storedTodos) setTodos(storedTodos);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos])
+
+  function addTodo(e) {
+    const val = inputNameVal.current.value;
+    if(val === '') return;
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name:val, complete:false}];
+    })
+    inputNameVal.current.value = '';
+  }
+
+  function toggleTodo(id) {
+    const copyTodos = [...todos];
+    const todo = copyTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(copyTodos);
+  }
+
+  function handleClearTodos() {
+    const uncompleteTodos = todos.filter(todo => !todo.complete)
+    setTodos(uncompleteTodos);
+  }
 
   return (
-    <div className="App">
-      <div>Rahul here</div>
-      <button onClick={() => updateCount(count + 1)}>Hit me!</button>
-      <Child counter={count} updateCounter={updateCount}/>
-    </div>
-  );
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={inputNameVal} type="text" />
+      <button onClick={addTodo}>Add</button>
+      <button onClick={handleClearTodos}>Clear Complete</button>
+      <div> {todos.filter(todo => !todo.complete).length} todo left</div>
+    </>
+  )
 }
-
-function Child(props) {
-  return (
-    <div>
-      <button onClick={() => props.updateCounter(props.counter + 1)}>Hit me!</button>
-      <div>{props.counter}</div>
-    </div>
-  );
-}
-
-
 
 export default App;
